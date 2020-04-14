a1c1fd75587ce997e18c0afa340f56be8b61156a
ltb <liangtb@qq.com>
2020-04-14 15:07:45 +0800

    feat: 完成theme dark和view zoomed

--- src/App.js
+++ src/App.js
@@ -2,40 +2,51 @@ import React, { Suspense } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { CircularProgress } from "@material-ui/core";
 import AccountLayout from "components/AccountLayout";
+import styles from "pages/index.module.css";
+import qs from "qs";
 
 const Support = React.lazy(() => import("pages/Support"));
 
 const loading = (
-    <div style={{ marginTop: "40vh", textAlign: "center" }}>
+    <div style={{ paddingTop: "40vh", textAlign: "center" }}>
         <CircularProgress />
     </div>
 );
 
 function App(props) {
+    const { search } = window.location;
+    const query = qs.parse(search, { ignoreQueryPrefix: true });
+
+    // eslint-disable-next-line no-unused-expressions
+    if (query.view === "zoomed") import("assets/zoomed.css");
+
     return (
-        <BrowserRouter basename="/oauth">
-            <Suspense fallback={loading}>
-                <Switch>
-                    <Route path="/support">
-                        <Support />
-                    </Route>
-                    <Route path="/account">
-                        <AccountLayout />
-                    </Route>
-                    <Route
-                        path="/"
-                        render={props => (
-                            <Redirect
-                                to={{
-                                    pathname: "/account",
-                                    search: props.location.search
-                                }}
-                            />
-                        )}
-                    />
-                </Switch>
-            </Suspense>
-        </BrowserRouter>
+        <>
+            {query.theme === "dark" && <div className={styles.dark} />}
+            <BrowserRouter basename="/oauth">
+                <Suspense fallback={loading}>
+                    <Switch>
+                        <Route path="/support">
+                            <Support />
+                        </Route>
+                        <Route path="/account">
+                            <AccountLayout />
+                        </Route>
+                        <Route
+                            path="/"
+                            render={props => (
+                                <Redirect
+                                    to={{
+                                        pathname: "/account",
+                                        search: props.location.search
+                                    }}
+                                />
+                            )}
+                        />
+                    </Switch>
+                </Suspense>
+            </BrowserRouter>
+        </>
     );
 }
 
